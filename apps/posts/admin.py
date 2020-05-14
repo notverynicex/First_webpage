4c466f7362b70dcd48919f503bddcf341267ec69
notverynicex <xnicenonikx@gmail.com>
2020-05-14 17:53:06 +0300

    Commit completed.

--- apps/posts/admin.py
+++ apps/posts/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
+from .models import Post
 # Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author', 'created_at', 'status']
+    list_filter = ['title', 'author', 'created_at']
+    search_fields = ['title', 'text']
+
+    def make_published(self, request, queryset):
+        queryset.update(status='p')
+
+    make_published.short_description = 'Mark selected post as published'
+admin.site.register(Post, PostAdmin)
